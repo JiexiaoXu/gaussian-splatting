bc8792f544939485850f6be96e2b41c13a540950
jiexiao <jiexiao@cs.washington.edu>
2024-10-08 10:31:55 -0700

    update the rasterizer

---
+++ experiement/color_eval.py
@@ -0,0 +1,12 @@
+# Get the color from ground truth
+
+
+# Get the color from model with sorting algorithm
+
+# Get the color from model with clustering algorithm
+
+# Compare the color from ground truth with the color from model with sorting algorithm
+
+# Compare the color from ground truth with the color from model with clustering algorithm
+
+# Plot the result
